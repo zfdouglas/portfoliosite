cb6f5451a865c52bbf7784917932b3b28d316bdd
ZFinn <puppybees@gmail.com>
2020-10-14 21:21:13 -0400

    Updates

---
+++ precache-manifest.4b3b9bc533d43e73670360aedaffe854.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c744cca8aeea9aa5576e3e937ac980d8",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "266711e83a8fff6a1ea9",
+    "url": "/portfoliosite/static/css/main.ae56c577.chunk.css"
+  },
+  {
+    "revision": "190f0e3b6badbd79dace",
+    "url": "/portfoliosite/static/js/2.63843126.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfoliosite/static/js/2.63843126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266711e83a8fff6a1ea9",
+    "url": "/portfoliosite/static/js/main.35ff2ff3.chunk.js"
+  },
+  {
+    "revision": "9bc02d9629cf4d3ac86f",
+    "url": "/portfoliosite/static/js/runtime-main.732b97db.js"
+  },
+  {
+    "revision": "a7f7aa43cccd61cb63c402b3d83931ba",
+    "url": "/portfoliosite/static/media/CaptnBill.a7f7aa43.png"
+  },
+  {
+    "revision": "99186ab871a18e73b18118810721a0cc",
+    "url": "/portfoliosite/static/media/LastSchanceext.99186ab8.png"
+  },
+  {
+    "revision": "bd9809516efef8440143a51fcc27e731",
+    "url": "/portfoliosite/static/media/Roomba.bd980951.png"
+  },
+  {
+    "revision": "347df0b7b1dd995ceeae300924c75d5a",
+    "url": "/portfoliosite/static/media/caret.347df0b7.svg"
+  },
+  {
+    "revision": "59d7df33b1e84745712922f3eab8d6b5",
+    "url": "/portfoliosite/static/media/disney.59d7df33.PNG"
+  },
+  {
+    "revision": "1704973166089ed84fb40ce7a37851d3",
+    "url": "/portfoliosite/static/media/lblogolong2.17049731.png"
+  },
+  {
+    "revision": "a5e1cffebfab6b290d026738ab61500b",
+    "url": "/portfoliosite/static/media/leafbackground.a5e1cffe.jpg"
+  },
+  {
+    "revision": "92d8a60238270dd023cc846861f36789",
+    "url": "/portfoliosite/static/media/leafbackgroundsmall.92d8a602.jpg"
+  },
+  {
+    "revision": "7a3e0684fad72cab0933bd0544282558",
+    "url": "/portfoliosite/static/media/mecircle.7a3e0684.png"
+  },
+  {
+    "revision": "c522b63171baf203d68ac6095b30f9f6",
+    "url": "/portfoliosite/static/media/munin.c522b631.PNG"
+  },
+  {
+    "revision": "a907a2a2feceaa7c46364d12c2b05297",
+    "url": "/portfoliosite/static/media/realzfd3.a907a2a2.png"
+  },
+  {
+    "revision": "ab4ab6e56b90461a4a12025405fb588f",
+    "url": "/portfoliosite/static/media/theland2.ab4ab6e5.png"
+  },
+  {
+    "revision": "fb166febded97fd2ecb09d36e8f23841",
+    "url": "/portfoliosite/static/media/thumbnail.fb166feb.png"
+  },
+  {
+    "revision": "03f98ba91d46b63f33e3a3aa3842f375",
+    "url": "/portfoliosite/static/media/umassseal.03f98ba9.png"
+  },
+  {
+    "revision": "b4bb368d9d5fc91a3a12af919ec4e5d7",
+    "url": "/portfoliosite/static/media/wellfed.b4bb368d.PNG"
+  }
+]);
